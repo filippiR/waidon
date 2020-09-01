517d7381d462acc9fc89a20a1f1466d7b48f2f43
Filippi Rizzi <filippi.rizzi@gmail.com>
2020-09-01 10:06:23 -0300

    Tradução

--- templates/Companies/add.php
+++ templates/Companies/add.php
@@ -8,16 +8,16 @@
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Companies'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('Lista Empresas'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="companies form content">
             <?= $this->Form->create($company) ?>
             <fieldset>
-                <legend><?= __('Add Company') ?></legend>
+                <legend><?= __('Cadastro de Empresas') ?></legend>
                 <?php
-                    echo $this->Form->control('name');
+                    echo $this->Form->control('name',['label' => __('Nome')]);
                 ?>
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
--- templates/Companies/index.php
+++ templates/Companies/index.php
@@ -5,16 +5,16 @@
  */
 ?>
 <div class="companies index content">
-    <?= $this->Html->link(__('New Company'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Companies') ?></h3>
+    <?= $this->Html->link(__('Nova Empresa'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+    <h3><?= __('Empresas') ?></h3>
     <div class="table-responsive">
         <table>
             <thead>
                 <tr>
                     <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
+                    <th><?= $this->Paginator->sort('name','Nome') ?></th>
+                    <th><?= $this->Paginator->sort('created','Criado') ?></th>
+                    <th><?= $this->Paginator->sort('modified', 'Modificado') ?></th>
                     <th class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
